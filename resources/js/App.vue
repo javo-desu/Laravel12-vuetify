8ccfdc55c6285c6ed03b178770a3839e1ea1607a
javo <javo@localhost.localdomain>
2025-05-26 10:13:09 -0400

    try1

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<><template>
+    <v-app>
+        <v-main>App
+            <v-container>
+                <h1>Hola desde Vue + Vuetify en Laravel 12</h1>
+                <v-btn color="primary">Botón Vuetify</v-btn>
+            </v-container>
+        </v-main>
+    </v-app>
+</template><script setup>
+// No hay nada más que hacer aquí en este ejemplo simple
+    </script></>
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './vuetify';
+
+createApp(App).use(vuetify).mount('#app');
